ffi/ffi

commit 75c20d3fa41243e7249e2d26873110dba0788108

Split out the closure pool from the method handles

ext/ffi_c/MethodHandle.c
@@ -51,6 +51,7 @@
 #include "Type.h"
 #include "LastError.h"
 #include "Call.h"
+#include "ClosurePool.h"
 #include "MethodHandle.h"
 
 
@@ -67,15 +68,6 @@
   typedef char* caddr_t;
 #endif
 
-#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32)
-#  define pool_lock(p) pthread_mutex_lock(&(p)->mutex)
-#  define pool_unlock(p)  pthread_mutex_unlock(&(p)->mutex)
-#else
-#  define pool_lock(p)
-#  define pool_unlock(p)
-#endif
-
-
 #ifdef USE_RAW
 #  define METHOD_CLOSURE ffi_raw_closure
 #  define METHOD_PARAMS ffi_raw*
@@ -84,146 +76,57 @@
 #  define METHOD_PARAMS void**
 #endif
 
-static void* allocatePage(void);
-static bool freePage(void *);
-static bool protectPage(void *);
 
-typedef void (*methodfn)(ffi_cif* cif, void* retval, METHOD_PARAMS parameters, void* user_data);
-static void attached_method_fast_invoke(ffi_cif* cif, void* retval, METHOD_PARAMS parameters, void* user_data);
-static void attached_method_invoke(ffi_cif* cif, void* retval, METHOD_PARAMS parameters, void* user_data);
-static ffi_status prep_trampoline(MethodHandle* method, char* errmsg, size_t errmsgsize);
+
+static bool prep_trampoline(void* ctx, void* code, Closure* closure, char* errmsg, size_t errmsgsize);
 static int trampoline_size(void);
 
 #if defined(__x86_64__) && defined(__GNUC__)
 # define CUSTOM_TRAMPOLINE 1
 #endif
 
+
 struct MethodHandle {
-    FunctionType* info;
-    void* function;
-    METHOD_CLOSURE* closure;
-    void* code;
-    ffi_cif cif;
-    struct MethodHandlePool* pool;
-    MethodHandle* next;
+    Closure* closure;
 };
 
-struct MethodHandlePool {
-#if defined (HAVE_NATIVETHREAD) && !defined(_WIN32)
-    pthread_mutex_t mutex;
-#endif
-    void (*fn)(ffi_cif* cif, void* retval, METHOD_PARAMS parameters, void* user_data);
-    MethodHandle* list;
-};
-
-static MethodHandlePool defaultMethodHandlePool, fastMethodHandlePool;
-static int pageSize;
+static ClosurePool* defaultClosurePool;
 
 
 MethodHandle*
 rbffi_MethodHandle_Alloc(FunctionType* fnInfo, void* function)
 {
-    MethodHandle* method, *list = NULL;
-    MethodHandlePool* pool;
-    caddr_t page;
-    int nclosures, trampolineSize;
-    int i, arity;
-
-    arity = fnInfo->parameterCount;
-    if (arity < 0) {
-        rb_raise(rb_eRuntimeError, "Cannot create method handle for variadic functions");
+    MethodHandle* handle;
+    Closure* closure = rbffi_Closure_Alloc(defaultClosurePool);
+    if (closure == NULL) {
+        rb_raise(rb_eNoMemError, "failed to allocate closure from pool");
         return NULL;
     }
 
-    if (false && arity <= MAX_METHOD_FIXED_ARITY && !fnInfo->blocking && !fnInfo->hasStruct && fnInfo->invoke == rbffi_CallFunction) {
-        pool = &fastMethodHandlePool;
-    } else {
-        pool = &defaultMethodHandlePool;
-    }
+    handle = xcalloc(1, sizeof(*handle));
+    handle->closure = closure;
+    closure->info = fnInfo;
+    closure->function = function;
 
-    pool_lock(pool);
-    if (pool->list != NULL) {
-        method = pool->list;
-        pool->list = pool->list->next;
-        pool_unlock(pool);
-        method->info = fnInfo;
-        method->function = function;
-
-        return method;
-    }
-
-    trampolineSize = roundup(trampoline_size(), 8);
-    nclosures = pageSize / trampolineSize;
-    page = allocatePage();
-    if (page == NULL) {
-        pool_unlock(pool);
-        rb_raise(rb_eRuntimeError, "failed to allocate a page. errno=%d (%s)", errno, strerror(errno));
-    }
-
-    for (i = 0; i < nclosures; ++i) {
-        char errmsg[256];
-        method = calloc(1, sizeof(MethodHandle));
-        if (method == NULL) {
-            snprintf(errmsg, sizeof(errmsg), "malloc failed: %s", strerror(errno));
-            goto error;
-        }
-        method->next = list;
-        list = method;
-        method->pool = pool;
-        method->code = method->closure = (METHOD_CLOSURE *) (page + (i * trampolineSize));
-
-        if (prep_trampoline(method, errmsg, sizeof(errmsg)) != FFI_OK) {
-            goto error;
-        }
-        
-        continue;
-error:
-        while (list != NULL) {
-            method = list;
-            list = list->next;
-            free(method);
-        }
-        freePage(page);
-        pool_unlock(pool);
-        rb_raise(rb_eRuntimeError, "%s", errmsg);
-    }
-    protectPage(page);
-
-    /* take the first member of the list for this handle */
-    method = list;
-    list = list->next;
-
-    /* now add the new block of MethodHandles to the pool */
-    if (list != NULL) {
-        list->next = pool->list;
-        pool->list = list;
-    }
-    pool_unlock(pool);
-    method->info = fnInfo;
-    method->function = function;
-
-    return method;
+    return handle;
 }
 
 void
-rbffi_MethodHandle_Free(MethodHandle* method)
+rbffi_MethodHandle_Free(MethodHandle* handle)
 {
-    if (method != NULL) {
-        MethodHandlePool* pool = method->pool;
-        pool_lock(pool);
-        method->next = pool->list;
-        pool->list = method;
-        pool_unlock(pool);
+    if (handle != NULL) {
+        rbffi_Closure_Free(handle->closure);
     }
 }
 
 void*
 rbffi_MethodHandle_CodeAddress(MethodHandle* handle)
 {
-    return handle->code;
+    return handle->closure->code;
 }
 
 #ifndef CUSTOM_TRAMPOLINE
+static void attached_method_invoke(ffi_cif* cif, void* retval, METHOD_PARAMS parameters, void* user_data);
 
 static ffi_type* methodHandleParamTypes[] = {
     &ffi_type_sint,
@@ -231,74 +134,31 @@ static ffi_type* methodHandleParamTypes[] = {
     &ffi_type_ulong,
 };
 
-static ffi_status
-prep_trampoline(MethodHandle* method, char* errmsg, size_t errmsgsize)
+static ffi_cif mh_cif;
+
+static bool
+prep_trampoline(void* ctx, void* code, Closure* closure, char* errmsg, size_t errmsgsize)
 {
-    ffi_status ffiStatus = ffi_prep_cif(&method->cif, FFI_DEFAULT_ABI, 3, &ffi_type_ulong,
-            methodHandleParamTypes);
-    if (ffiStatus != FFI_OK) {
-        snprintf(errmsg, errmsgsize, "ffi_prep_cif failed.  status=%#x", ffiStatus);
-        return ffiStatus;
-    }
+    ffi_status ffiStatus;
 
 #if defined(USE_RAW)
-    ffiStatus = ffi_prep_raw_closure(method->closure, &method->cif, method->pool->fn, method);
+    ffiStatus = ffi_prep_raw_closure(code, &mh_cif, attached_method_invoke, closure);
 #else
-    ffiStatus = ffi_prep_closure(method->closure, &method->cif, method->pool->fn, method);
+    ffiStatus = ffi_prep_closure(code, &mh_cif, attached_method_invoke, closure);
 #endif
     if (ffiStatus != FFI_OK) {
         snprintf(errmsg, errmsgsize, "ffi_prep_closure failed.  status=%#x", ffiStatus);
-        return ffiStatus;
+        return false;
     }
 
-    return FFI_OK;
+    return true;
 }
+
 
 static int
 trampoline_size(void)
 {
     return sizeof(METHOD_CLOSURE);
-}
-
-#endif
-/*
- * attached_method_invoke is used as the <= MAX_METHOD_FIXED_ARITY argument fixed-arity fast path
- */
-static void
-attached_method_fast_invoke(ffi_cif* cif, void* mretval, METHOD_PARAMS parameters, void* user_data)
-{
-    MethodHandle* handle =  (MethodHandle *) user_data;
-    FunctionType* fnInfo = handle->info;
-    void* ffiValues[MAX_METHOD_FIXED_ARITY];
-    FFIStorage params[MAX_METHOD_FIXED_ARITY], retval;
-
-
-    if (fnInfo->parameterCount > 0) {
-#ifdef USE_RAW
-        int argc = parameters[0].sint;
-        VALUE* argv = *(VALUE **) & parameters[1];
-#else
-        int argc = *(ffi_sarg *) parameters[0];
-        VALUE* argv = *(VALUE **) parameters[1];
-#endif
-
-        rbffi_SetupCallParams(argc, argv,
-                fnInfo->parameterCount, fnInfo->nativeParameterTypes, params, ffiValues,
-                fnInfo->callbackParameters, fnInfo->callbackCount, fnInfo->rbEnums);
-    }
-
-#ifdef USE_RAW
-    ffi_raw_call(&fnInfo->ffi_cif, FFI_FN(handle->function), &retval, (ffi_raw *) ffiValues[0]);
-#else
-    ffi_call(&fnInfo->ffi_cif, FFI_FN(handle->function), &retval, ffiValues);
-#endif
-
-    if (!fnInfo->ignoreErrno) {
-        rbffi_save_errno();
-    }
-
-    *((VALUE *) mretval) = rbffi_NativeValue_ToRuby(fnInfo->returnType, fnInfo->rbReturnType,
-        &retval, fnInfo->rbEnums);
 }
 
 /*
@@ -308,8 +168,9 @@ attached_method_fast_invoke(ffi_cif* cif, void* mretval, METHOD_PARAMS parameter
 static void
 attached_method_invoke(ffi_cif* cif, void* mretval, METHOD_PARAMS parameters, void* user_data)
 {
-    MethodHandle* handle =  (MethodHandle *) user_data;
-    
+    Closure* handle =  (Closure *) user_data;
+    FunctionType* fnInfo = (FunctionType *) handle->info;
+
 #ifdef USE_RAW
     int argc = parameters[0].sint;
     VALUE* argv = *(VALUE **) &parameters[1];
@@ -318,13 +179,17 @@ attached_method_invoke(ffi_cif* cif, void* mretval, METHOD_PARAMS parameters, vo
     VALUE* argv = *(VALUE **) parameters[1];
 #endif
 
-    *(VALUE *) mretval = (*handle->info->invoke)(argc, argv, handle->function, handle->info);
+    *(VALUE *) mretval = (*fnInfo->invoke)(argc, argv, handle->function, fnInfo);
 }
+
+#endif
+
+
 
 #if defined(CUSTOM_TRAMPOLINE)
 #if defined(__x86_64__)
 
-static VALUE custom_trampoline(int argc, VALUE* argv, VALUE self, MethodHandle*);
+static VALUE custom_trampoline(int argc, VALUE* argv, VALUE self, Closure*);
 
 #define TRAMPOLINE_CTX_MAGIC (0xfee1deadcafebabe)
 #define TRAMPOLINE_FUN_MAGIC (0xfeedfacebeeff00d)
@@ -355,14 +220,15 @@ asm(
 );
 
 static VALUE
-custom_trampoline(int argc, VALUE* argv, VALUE self, MethodHandle* handle)
+custom_trampoline(int argc, VALUE* argv, VALUE self, Closure* handle)
 {
-    return (*handle->info->invoke)(argc, argv, handle->function, handle->info);
+    FunctionType* fnInfo = (FunctionType *) handle->info;
+    return (*fnInfo->invoke)(argc, argv, handle->function, fnInfo);
 }
 
 #elif defined(__i386__) && 0
 
-static VALUE custom_trampoline(caddr_t args, MethodHandle*);
+static VALUE custom_trampoline(caddr_t args, Closure*);
 #define TRAMPOLINE_CTX_MAGIC (0xfee1dead)
 #define TRAMPOLINE_FUN_MAGIC (0xbeefcafe)
 
@@ -394,9 +260,10 @@ asm(
 );
 
 static VALUE
-custom_trampoline(caddr_t args, MethodHandle* handle)
+custom_trampoline(caddr_t args, Closure* handle)
 {
-    return (*handle->info->invoke)(*(int *) args, *(VALUE **) (args + 4), handle->function, handle->info);
+    FunctionType* fnInfo = (FunctionType *) handle->info;
+    return (*fnInfo->invoke)(*(int *) args, *(VALUE **) (args + 4), handle->function, fnInfo);
 }
 
 #endif /* __x86_64__ else __i386__ */
@@ -436,17 +303,17 @@ trampoline_offsets(int* ctxOffset, int* fnOffset)
     return 0;
 }
 
-static ffi_status
-prep_trampoline(MethodHandle* method, char* errmsg, size_t errmsgsize)
+static bool
+prep_trampoline(void* ctx, void* code, Closure* closure, char* errmsg, size_t errmsgsize)
 {
-    caddr_t ptr = (caddr_t) method->code;
+    caddr_t ptr = (caddr_t) code;
 
     memcpy(ptr, &ffi_trampoline, trampoline_size());
     // Patch the context and function addresses into the stub code
-    *(intptr_t *)(ptr + trampoline_ctx_offset) = (intptr_t) method;
+    *(intptr_t *)(ptr + trampoline_ctx_offset) = (intptr_t) closure;
     *(intptr_t *)(ptr + trampoline_func_offset) = (intptr_t) custom_trampoline;
 
-    return FFI_OK;
+    return true;
 }
 
 static int
@@ -457,66 +324,23 @@ trampoline_size(void)
 
 #endif /* CUSTOM_TRAMPOLINE */
 
-static int
-getPageSize()
-{
-#ifdef _WIN32
-    SYSTEM_INFO si;
-    GetSystemInfo(&si);
-    return si.dwPageSize;
-#else
-    return sysconf(_SC_PAGESIZE);
-#endif
-}
-
-static void*
-allocatePage(void)
-{
-#ifdef _WIN32
-    return VirtualAlloc(NULL, pageSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-#else
-    caddr_t page = mmap(NULL, pageSize, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-    return (page != (caddr_t) -1) ? page : NULL;
-#endif
-}
-
-static bool
-freePage(void *addr)
-{
-#ifdef _WIN32
-    return VirtualFree(addr, 0, MEM_RELEASE);
-#else
-    return munmap(addr, pageSize) == 0;
-#endif
-}
-
-static bool
-protectPage(void* page)
-{
-#ifdef _WIN32
-    DWORD oldProtect;
-    return VirtualProtect(page, pageSize, PAGE_EXECUTE_READ, &oldProtect);
-#else
-    return mprotect(page, pageSize, PROT_READ | PROT_EXEC) == 0;
-#endif
-}
 
 void
 rbffi_MethodHandle_Init(VALUE module)
 {
-    pageSize = getPageSize();
-
-#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32)
-    pthread_mutex_init(&defaultMethodHandlePool.mutex, NULL);
-    pthread_mutex_init(&fastMethodHandlePool.mutex, NULL);
-#endif /* USE_PTHREAD_LOCAL */
-    defaultMethodHandlePool.fn = attached_method_invoke;
-    fastMethodHandlePool.fn = attached_method_fast_invoke;
+    defaultClosurePool = rbffi_ClosurePool_New(trampoline_size(), prep_trampoline, NULL);
 
 #if defined(CUSTOM_TRAMPOLINE)
     if (trampoline_offsets(&trampoline_ctx_offset, &trampoline_func_offset) != 0) {
         rb_raise(rb_eFatal, "Could not locate offsets in trampoline code");
     }
+#else
+    ffi_status ffiStatus = ffi_prep_cif(&mh_cif, FFI_DEFAULT_ABI, 3, &ffi_type_ulong,
+            methodHandleParamTypes);
+    if (ffiStatus != FFI_OK) {
+        rb_raise(rb_eFatal, "ffi_prep_cif failed.  status=%#x", ffiStatus);
+    }
+
 #endif
 }
 

ext/ffi_c/ClosurePool.c
@@ -1,0 +1,280 @@
+/*
+ * Copyright (c) 2009, Wayne Meissner
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * The name of the author or authors may not be used to endorse or promote
+ *   products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/param.h>
+#include <sys/types.h>
+#ifndef _WIN32
+#  include <sys/mman.h>
+#endif
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#ifndef _WIN32
+#  include <unistd.h>
+#endif
+#include <errno.h>
+#include <ruby.h>
+#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32) && !defined(__WIN32__)
+# include <pthread.h>
+#endif
+
+#include <ffi.h>
+#include "rbffi.h"
+#include "compat.h"
+
+#include "Function.h"
+#include "Types.h"
+#include "Type.h"
+#include "LastError.h"
+#include "Call.h"
+
+#include "ClosurePool.h"
+
+
+#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32) && !defined(__WIN32__)
+#  define USE_PTHREAD_LOCAL
+#endif
+
+#ifndef roundup
+#  define roundup(x, y)   ((((x)+((y)-1))/(y))*(y))
+#endif
+#ifdef _WIN32
+  typedef char* caddr_t;
+#endif
+
+typedef struct Memory {
+    void* code;
+    void* data;
+    struct Memory* next;
+} Memory;
+
+struct ClosurePool_ {
+    void* ctx;
+    int closureSize;
+    bool (*prep)(void* ctx, void *code, Closure* closure, char* errbuf, size_t errbufsize);
+#if defined (HAVE_NATIVETHREAD) && !defined(_WIN32)
+    pthread_mutex_t mutex;
+#endif
+    struct Memory* blocks; /* Keeps track of all the allocated memory for this pool */
+    Closure* list;
+};
+
+#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32)
+#  define pool_lock(p) pthread_mutex_lock(&(p)->mutex)
+#  define pool_unlock(p)  pthread_mutex_unlock(&(p)->mutex)
+#else
+#  define pool_lock(p)
+#  define pool_unlock(p)
+#endif
+
+static int pageSize;
+
+static void* allocatePage(void);
+static bool freePage(void *);
+static bool protectPage(void *);
+
+ClosurePool*
+rbffi_ClosurePool_New(int closureSize, 
+        bool (*prep)(void* ctx, void *code, Closure* closure, char* errbuf, size_t errbufsize),
+        void* ctx)
+{
+    ClosurePool* pool;
+
+    pool = xcalloc(1, sizeof(*pool));
+    pool->closureSize = closureSize;
+    pool->ctx = ctx;
+    pool->prep = prep;
+    
+#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32) && !defined(__WIN32__)
+    pthread_mutex_init(&pool->mutex, NULL);
+#endif
+
+    return pool;
+}
+
+void
+rbffi_ClosurePool_Free(ClosurePool* pool)
+{
+    Memory* memory;
+
+    if (pool != NULL) {
+        for (memory = pool->blocks; memory != NULL; ) {
+            Memory* next = memory;
+            freePage(memory->code);
+            free(memory->data);
+            free(memory);
+            memory = next;
+        }
+        free(pool);
+    }
+
+}
+
+Closure*
+rbffi_Closure_Alloc(ClosurePool* pool)
+{
+    Closure *list = NULL;
+    Memory* block = NULL;
+    caddr_t code = NULL;
+    char errmsg[256];
+    int nclosures, trampolineSize;
+    int i;
+
+    pool_lock(pool);
+    if (pool->list != NULL) {
+        Closure* closure = pool->list;
+        pool->list = pool->list->next;
+        pool_unlock(pool);
+
+        return closure;
+    }
+
+    trampolineSize = roundup(pool->closureSize, 8);
+    nclosures = pageSize / trampolineSize;
+    block = calloc(1, sizeof(*block));
+    list = calloc(nclosures, sizeof(*list));
+    code = allocatePage();
+    
+    if (block == NULL || list == NULL || code == NULL) {
+        pool_unlock(pool);
+        snprintf(errmsg, sizeof(errmsg), "failed to allocate a page. errno=%d (%s)", errno, strerror(errno));
+        goto error;
+    }
+    
+    for (i = 0; i < nclosures; ++i) {
+        Closure* closure = &list[i];
+        closure->next = &list[i + 1];
+        closure->pool = pool;
+        closure->code = (code + (i * trampolineSize));
+
+        if (!(*pool->prep)(pool->ctx, closure->code, closure, errmsg, sizeof(errmsg))) {
+            goto error;
+        }
+    }
+
+    if (!protectPage(code)) {
+        goto error;
+    }
+
+    /* Track the allocated page + Closure memory area */
+    block->data = list;
+    block->code = code;
+    block->next = pool->blocks;
+    pool->blocks = block;
+
+    /* Thread the new block onto the free list, apart from the first one. */
+    list[nclosures - 1].next = pool->list;
+    pool->list = list->next;
+
+    pool_unlock(pool);
+
+    /* Use the first one as the new handle */
+    return list;
+
+error:
+    pool_unlock(pool);
+    free(block);
+    free(list);
+    if (code != NULL) {
+        freePage(code);
+    }
+    
+
+    rb_raise(rb_eRuntimeError, "%s", errmsg);
+    return NULL;
+}
+
+void
+rbffi_Closure_Free(Closure* closure)
+{
+    if (closure != NULL) {
+        ClosurePool* pool = closure->pool;
+        pool_lock(pool);
+        // Just push it on the front of the free list
+        closure->next = pool->list;
+        pool->list = closure;
+        pool_unlock(pool);
+    }
+}
+
+void*
+rbffi_Closure_CodeAddress(Closure* handle)
+{
+    return handle->code;
+}
+
+
+static int
+getPageSize()
+{
+#ifdef _WIN32
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    return si.dwPageSize;
+#else
+    return sysconf(_SC_PAGESIZE);
+#endif
+}
+
+static void*
+allocatePage(void)
+{
+#ifdef _WIN32
+    return VirtualAlloc(NULL, pageSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+#else
+    caddr_t page = mmap(NULL, pageSize, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    return (page != (caddr_t) -1) ? page : NULL;
+#endif
+}
+
+static bool
+freePage(void *addr)
+{
+#ifdef _WIN32
+    return VirtualFree(addr, 0, MEM_RELEASE);
+#else
+    return munmap(addr, pageSize) == 0;
+#endif
+}
+
+static bool
+protectPage(void* page)
+{
+#ifdef _WIN32
+    DWORD oldProtect;
+    return VirtualProtect(page, pageSize, PAGE_EXECUTE_READ, &oldProtect);
+#else
+    return mprotect(page, pageSize, PROT_READ | PROT_EXEC) == 0;
+#endif
+}
+
+void
+rbffi_ClosurePool_Init(VALUE module)
+{
+    pageSize = getPageSize();
+}
+

ext/ffi_c/ClosurePool.h
@@ -1,0 +1,28 @@
+#ifndef RUBYFFI_CLOSUREPOOL_H
+#define RUBYFFI_CLOSUREPOOL_H
+
+typedef struct ClosurePool_ ClosurePool;
+typedef struct Closure_ Closure;
+
+struct Closure_ {
+    void* info;      /* opaque handle for storing closure-instance specific data */
+    void* function;  /* closure-instance specific function, called by custom trampoline */
+    void* code;      /* The native trampoline code location */
+    struct ClosurePool_* pool;
+    Closure* next;
+};
+
+void rbffi_ClosurePool_Init(VALUE module);
+
+ClosurePool* rbffi_ClosurePool_New(int closureSize, 
+        bool (*prep)(void* ctx, void *code, Closure* closure, char* errbuf, size_t errbufsize),
+        void* ctx);
+
+void rbffi_ClosurePool_Free(ClosurePool *);
+
+Closure* rbffi_Closure_Alloc(ClosurePool *);
+void rbffi_Closure_Free(Closure *);
+
+void* rbffi_Closure_GetCodeAddress(Closure *);
+
+#endif /* RUBYFFI_CLOSUREPOOL_H */

ext/ffi_c/ffi.c
@@ -43,6 +43,7 @@
 #include "Types.h"
 #include "LastError.h"
 #include "Function.h"
+#include "ClosurePool.h"
 #include "MethodHandle.h"
 #include "Call.h"
 #include "ArrayType.h"
@@ -63,6 +64,7 @@ Init_ffi_c(void) {
     rbffi_ArrayType_Init(moduleFFI);
     rbffi_LastError_Init(moduleFFI);
     rbffi_Call_Init(moduleFFI);
+    rbffi_ClosurePool_Init(moduleFFI);
     rbffi_MethodHandle_Init(moduleFFI);
     rbffi_Platform_Init(moduleFFI);
     rbffi_AbstractMemory_Init(moduleFFI);

forgottenswitch/rbffi

commit e31d8dccc720d24c419f087f625dad427819d9e9

Fix memory leak in MethodHandle
Introduced in 75c20d3fa41243e7249e2d26873110dba0788108

ext/ffi_c/MethodHandle.c
@@ -102,6 +102,7 @@ rbffi_MethodHandle_Free(MethodHandle* handle)
     if (handle != NULL) {
         rbffi_Closure_Free(handle->closure);
     }
+    xfree(handle);
 }
 
 void*